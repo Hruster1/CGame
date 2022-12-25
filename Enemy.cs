7a058b796d2127fa0bc08750672d2ed11dcf8ef7
Hrust <96692587+Hruster1@users.noreply.github.com>
2022-12-25 13:38:03 +0300

    Create Enemy.cs

---
+++ Enemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Скорость движения врага
+    public float speed;
+    //Цель, к которой движется враг
+    public Transform target;
+    //Очки урона от атаки врагом игрока
+    public int playerDamage = 2;
+
+    void Update()
+    {
+        //Меняет каждый кадр позицию NPC на новую
+        transform.position =
+        Vector3.MoveTowards(transform.position,
+        target.position, speed * Time.deltaTime);
+        //Разворачивает каждый кадр NPC лицом к цели
+        transform.LookAt(target.position);
+    }
+            
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.GetComponent<Player>();
+        player.TakeDamage(playerDamage);
+    }
+
+}
